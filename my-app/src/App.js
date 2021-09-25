73d086d7fee26c301ed1012dc8f7ca0a2b955c4f
John Traas <j.j.h.traas@gmail.com>
2021-09-25 22:52:46 +0000

    add component to app - Sidebar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import css from './App.module.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
+      <Sidebar word="World" first="Photos" second="Illustrations" third="Paintings"/>
     </div>
   );
 }
