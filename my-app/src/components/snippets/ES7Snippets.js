d070bff9b0df6d28cf25289e25923671f02ca5c0
John Traas <j.j.h.traas@gmail.com>
2021-09-29 10:34:44 +0000

    add component - ES7Snippets.js

---
+++ my-app/src/components/snippets/ES7Snippets.js
@@ -0,0 +1,57 @@
+// rfce SNIPPET create/export stateless component
+
+// import React from 'react'
+
+// function ES7Snippets() {
+//     return (
+//         <div>
+            
+//         </div>
+//     )
+// }
+
+// export default ES7Snippets
+
+// rafce SNIPPET create/export component as arrow function
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//     return (
+//         <div>
+            
+//         </div>
+//     )
+// }
+
+// export default ES7Snippets
+
+
+// rce SNIPPET create/export class component
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//     render() {
+//         return (
+//             <div>
+                
+//             </div>
+//         )
+//     }
+// }
+
+// export default ES7Snippets
+
+// rconst SNIPPET create constructor
+// constructor(props) {
+//     super(props)
+
+//     this.state = {
+         
+//     }
+// }
+
+// clg SNIPPET log object to console
+// console.log(object)
+
+// clo SNIPPET log object along with string of same name
+// console.log(`object`, object)
