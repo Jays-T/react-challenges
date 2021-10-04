9dafc0c9da6c53ea3aaaa7eb600ce4c04992bf53
John Traas <j.j.h.traas@gmail.com>
2021-10-04 15:40:19 +0000

    testing conditional rendering components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,8 @@ import EventsClass from './components/EventsClass';
 import EventsFunctional from './components/EventsFunctional';
 import NavBarSimple from './components/NavBarSimple';
 import Sidebar from './components/Sidebar';
+import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
+import ConditionalRenderingClass from './components/ConditionalRenderingClass';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       {/* Add your components here */}
       <NavBarSimple />
       <Sidebar word="World" first="Photos" second="Illustrations" third="Paintings"/>
+      <ConditionalRenderingFunctional connected={true} />
+      <ConditionalRenderingClass isLoaded={false} />
       <EventsFunctional />
       <EventsClass />
     </div>
