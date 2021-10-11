21000d54471cfc534493a03e118aea48ddc4b24e
John Traas <j.j.h.traas@gmail.com>
2021-10-11 14:34:33 +0000

    update component PostItem with sentence+paragraph keys

--- my-app/src/components/PostItem.js
+++ my-app/src/components/PostItem.js
@@ -6,13 +6,15 @@ const PostItem = (props) => {
     return (
         props.savedPosts.map(post => {
 
-            const {name, title, description, image} = post;
+            const {name, title, sentence, paragraph, description, image} = post;
             return(
                 <div key={title} className = {css.SearchItem}>
                     <p>{title}</p>
                     <p>{name}</p>
+                    <p>{sentence}</p>
                     <img src={image} alt={title}></img>
                     <p>{description}</p>
+                    <p>{paragraph}</p>
                 </div>
             )
         })
