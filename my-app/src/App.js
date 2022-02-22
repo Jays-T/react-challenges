f98f8a6e1f33860f1bf1c26c916927bff401593c
John Traas <j.j.h.traas@gmail.com>
2022-02-22 19:38:31 +0000

    ADD ControlledForm + SearchBar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,16 @@
 import css from './App.module.css';
-import EventsClass from './components/EventsClass';
-import EventsFunctional from './components/EventsFunctional';
-import NavBarSimple from './components/NavBarSimple';
+// import EventsClass from './components/EventsClass';
+// import EventsFunctional from './components/EventsFunctional';
+// import NavBarSimple from './components/NavBarSimple';
 import Sidebar from './components/Sidebar';
-import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
-import ConditionalRenderingClass from './components/ConditionalRenderingClass';
-import NestingComponents from './components/NestingComponents';
+// import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
+// import ConditionalRenderingClass from './components/ConditionalRenderingClass';
+// import NestingComponents from './components/NestingComponents';
 import NavBar from './components/NavBar';
-import RenderList from './components/RenderList';
+// import RenderList from './components/RenderList';
 import Content from './components/Content';
+import ControlledForm from './components/ControlledForm';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
       {/* <NavBarSimple /> */}
       <NavBar />
       <Sidebar word="World" first="Photos" second="Illustrations" third="Paintings"/>
+      <ControlledForm />
+      <SearchBar />
       <Content />
       {/* <ConditionalRenderingFunctional connected={true} /> */}
       {/* <ConditionalRenderingClass isLoaded={false} /> */}
